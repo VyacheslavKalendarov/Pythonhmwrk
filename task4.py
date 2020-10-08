67c2b885c368685f6f7b09c656d6cf6671644e24
VyacheslavKalendarov <71467872+VyacheslavKalendarov@users.noreply.github.com>
2020-10-08 21:08:13 +0300

    hmwrk5

---
+++ task4.py
@@ -0,0 +1,22 @@
+'''Создать (не программно) текстовый файл со следующим содержимым:
+One — 1
+Two — 2
+Three — 3
+Four — 4
+Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные.
+При этом английские числительные должны заменяться на русские.
+Новый блок строк должен записываться в новый текстовый файл.'''
+
+with open('fourth.txt', 'w', encoding='utf-8') as f:
+    f.writelines('One — 1\nTwo — 2\nThree — 3\nFour — 4')
+
+rus = {'One' : 'Один', 'Two' : 'Два', 'Three' : 'Три', 'Four' : 'Четыре'}
+new_file = []
+with open('fourth.txt', 'r', encoding='utf-8') as f:
+        for i in f:
+            i = i.split(' ', 1)
+            new_file.append(rus[i[0]] + '  ' + i[1])
+        print(new_file)
+
+with open('fourth_new.txt', 'w',encoding='utf-8') as f2:
+    f2.writelines(new_file)
